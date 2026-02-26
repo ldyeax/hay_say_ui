ec0a04564907761f035b79acacf09df3bc0b8c07
jimm <jimm@jimm.horse>
2026-02-26 23:19:26 +0000

    setup_luna

--- ubuntuserver/setup_luna.sh
+++ ubuntuserver/setup_luna.sh
@@ -2,8 +2,11 @@ export LIMITED_USER=luna
 export HOME_DIR=/home/$LIMITED_USER
 
 cd $HOME_DIR
-rm -rf ./.venv
-uv venv --seed
+if [ -d ".venv" ]; then
+	echo "venv exists"
+else
+	uv venv --seed
+fi
 source .venv/bin/activate
 
 pip install -r requirements.txt
@@ -18,17 +21,5 @@ if [ -d "hay_say_ui" ]; then
 	git submodule update --init --recursive
 	git pull
 else
-	git clone -b main --single-branch -q https://github.com/ldyeax/hay_say_ui ~/hay_say/hay_say_ui/
+	git clone --recursive https://github.com/ldyeax/hay_say_ui
 fi
-
-cd $HOME_DIR
-
-# Expose port 6573, the port that Hay Say uses
-#EXPOSE 6573
-
-# Start Celery workers for background callbacks and run Hay Say on a gunicorn server
-#CMD ["/bin/sh", "-c", " \
-celery --workdir ~/hay_say/hay_say_ui/ -A celery_download:celery_app worker --loglevel=INFO --concurrency 5 & \
-celery --workdir ~/hay_say/hay_say_ui/ -A celery_generate_gpu:celery_app worker --loglevel=INFO --concurrency 1 & \
-celery --workdir ~/hay_say/hay_say_ui/ -A celery_generate_cpu:celery_app worker --loglevel=INFO --concurrency 1 & \
-gunicorn --config=server_initialization.py --workers 1 --bind 0.0.0.0:6573 'wsgi:get_server()'
