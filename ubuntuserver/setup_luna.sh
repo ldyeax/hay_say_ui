7ec22c5a5c5ed3a3df11ede60dca92597cf9353d
jimm <jimm@jimm.horse>
2026-02-27 17:53:13 -0600

    setup luna

--- ubuntuserver/setup_luna.sh
+++ ubuntuserver/setup_luna.sh
@@ -1,6 +1,8 @@
-SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+SCRIPT_DIR="/home/luna/hay_say/hay_say_ui/ubuntuserver"
 source "$SCRIPT_DIR/variables.sh"
-cd $HOME_DIR
+echo Moving into HOME_DIR=$HOME_DIR
+cd "$HOME_DIR"
+echo HOME_DIR PWD=$PWD
 if [ -d ".venv" ]; then
 	echo "venv exists"
 else
@@ -12,15 +14,20 @@ source .venv/bin/activate
 export PATH="$PATH:$HOME_DIR/.local/bin"
 
 # Clone Hay Say
-mkdir -p $HOME_DIR/hay_say
-cd $HOME_DIR/hay_say
+hay_say="$HOME_DIR/hay_say"
+echo Creating hay_say at $hay_say
+mkdir -p $hay_say
+cd "$hay_say"
+echo PWD is $PWD
 if [ -d "hay_say_ui" ]; then
 	cd hay_say_ui
+	echo Found existing hay_say_ui at $PWD
 	git submodule update --init --recursive
 	git pull
 else
 	git clone --recursive https://github.com/ldyeax/hay_say_ui
 	cd hay_say_ui
+	echo Cloned hay_say_ui into $PWD
 fi
 
 cd ubuntuserver
